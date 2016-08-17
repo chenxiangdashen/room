91eca4c14c6ba0ae705f534fa9eb73eed00c98ca
chenxiangdashen <chenxiangdashen123@163.com>
2016-08-17 20:50:07 +0800

    Signed-off-by: chenxiangdashen <chenxiangdashen123@163.com>

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -1,54 +1,84 @@
-var mysql=require('mysql');
+var mysql = require('mysql');
 
-function connectServer(){
+function connectServer() {
 	var client = mysql.createConnection({
-		host:'localhost',
-		user:'test',
-		password:'',
-		database:'test'
+		host: 'localhost',
+		user: 'test',
+		password: '',
+		database: 'test'
 	});
 	return client;
 }
 
+function selectFun(client, username, callback) {
+	//client为一个mysql连接对象
+	client.query('select password from users where name="' + username + '"', function(err, results, fields) {
+		if (err) throw err;
 
- function  selectFun(client,username,callback){
-     //client为一个mysql连接对象
-    client.query('select password from users where name="'+username+'"',function(err,results,fields){
-         if(err) throw err;
-
-         callback(results);
-     });
- }
-
- function insertRoom(room,callback){
-
- 	var connection = mysql.createConnection({    
-	  host:'localhost',
-		user:'test',
-		password:'',
-		database:'test'
+		callback(results);
 	});
-	 
-	connection.connect();
-	var  roomAddSql = 'INSERT INTO room(id,roomName,cate,buzz,image) VALUES(?,?,?,?,?)';
-	var  roomAddSql_Params = [room.id, room.roomName, room.cate, room.buzz, room.image];
-	//增 add
-	console.log(roomAddSql_Params)
-	// client.connect();
-	connection.query(roomAddSql,roomAddSql_Params,function (err, result) {
-	        if(err){
-	         console.log('[INSERT ERROR] - ',err.message);
-	         return;
-	        }       
-	       console.log('-------INSERT----------');
-	       //console.log('INSERT ID:',result.insertId);       
-	       console.log('INSERT ID:',result);       
-	       console.log('#######################'); 
-	       callback(result)
+}
+
+function insertRoom(room, callback) {
+
+	//	var connection = mysql.createConnection({
+	//		host: 'localhost',
+	//		user: 'test',
+	//		password: '',
+	//		database: 'test'
+	//	});
+
+	var pool = mysql.createPool({
+		host: 'localhost',
+		user: 'test',
+		password: '',
+		database: 'test'
 	});
-	connection.end();
-	
- }
+
+	pool.getConnection(function(err, connection) {
+		if (err) throw err;
+
+		connection.connect();
+		var roomAddSql = 'INSERT INTO room(id,roomName,cate,buzz,image) VALUES(?,?,?,?,?)';
+		var roomAddSql_Params = [room.id, room.roomName, room.cate, room.buzz, room.image];
+		// 执行sql
+		connection.query(roomAddSql, roomAddSql_Params, function(err, result) {
+			connection.release();
+			connection.end();
+			if (err) {
+				console.log('[INSERT ERROR] - ', err.message);
+				return;
+			}
+			//	       console.log('-------INSERT----------');
+			//console.log('INSERT ID:',result.insertId);       
+			//	       console.log('INSERT ID:',result);       
+			//	       console.log('#######################'); 
+			callback(result)
+		});
+		// 释放此连接
+		
+		
+	});
+
+//	connection.connect();
+//	var roomAddSql = 'INSERT INTO room(id,roomName,cate,buzz,image) VALUES(?,?,?,?,?)';
+//	var roomAddSql_Params = [room.id, room.roomName, room.cate, room.buzz, room.image];
+//	//增 add
+//	// client.connect();
+//	connection.query(roomAddSql, roomAddSql_Params, function(err, result) {
+//		if (err) {
+//			console.log('[INSERT ERROR] - ', err.message);
+//			return;
+//		}
+//		//	       console.log('-------INSERT----------');
+//		//console.log('INSERT ID:',result.insertId);       
+//		//	       console.log('INSERT ID:',result);       
+//		//	       console.log('#######################'); 
+//		callback(result)
+//	});
+//	connection.end();
+
+}
 
 exports.connect = connectServer;
 exports.selectFun = selectFun;
--- db/pachong.js
+++ db/pachong.js
@@ -1,65 +1,144 @@
-//var request = require('request');
- var request = require('request-json');
+var request = require('request');
 var cheerio = require('cheerio');
 var async = require('async');
-var roomModle=require('./dbConnect');
+var roomModle = require('./dbConnect');
 
 exports.get = function() {
-    var url = 'http://www.panda.tv/live_lists?status=2&order=person_num&token=b902c644602f7b1ca7c6b2e0732f0d49&pageno=1&pagenum=120&classification=&_=1470552304423';
-    var url_list = [];
-    var list = [];
+	var url = 'http://www.panda.tv/live_lists?status=2&order=person_num&pagenum=120&token=b902c644602f7b1ca7c6b2e0732f0d49&classification=';
+	var url_list = [];
+	var list = [];
 
+	async.series({
+		T1: function(callback) {
+			request({
+				url: url,
+				data: {
+					pageno: 1
+				}
+			}, function(err, data, body) {
+				if (err) return console.error(err);
+				var total = JSON.parse(body).data.total;
 
+				for (var a = 1; a <= total / 120; a++) {
+					var uu = 'http://www.panda.tv/live_lists?status=2&order=person_num&pagenum=120&token=b902c644602f7b1ca7c6b2e0732f0d49&classification=&pageno=' + a;
+					url_list.push(uu);
+				}
 
-    url_list.push(url);
-    
-    
-   
+				callback();
+			});
+		},
+		T2: function(callback) {
+			async.eachSeries(url_list, function(arr_url, callback) {
+				console.log('正在抓取' + arr_url + '的数据...');
+				request({
+					url: arr_url
+				}, function(err, data, body) {
+					if (err) return console.error(err);
+					var roomList = JSON.parse(body).data.items
+					for (var i = 0; i < roomList.length; i++) {
+						var _this = roomList[i];
+						var item = {
+							roomName: _this.userinfo.nickName,
+							id: _this.id,
+							buzz: _this.person_num,
+							cate: _this.name,
+							image: _this.pictures.img
+						}
 
-    async.eachSeries(url_list, function(arr_url, callback) {
-    
-       console.log('正在抓取' + arr_url + '的数据...');
-       
-       var client = request.newClient(arr_url);
+						list.push(item);
+					}
+					callback(err, list);
+				});
 
-		var data = {};
-		client.get('Config', data, function(err, res, body) {
-		  console.log(res.statusCode,body);
-		});
+			}, function(err) {
+				if (err) return console.error(err.stack);
+				
+				callback();
+			});
+			
+		},
+		T3:function(callback){
+			
+			for ( var i =0 ; i<list.length;i++ ){
+				console.log('正在写入第 ----'+ i+'--------------' + list[i].roomName + '的数据...');
+				
+					roomModle.insertRoom(list[i], function(result) {
+						if (result.affectedRows == 1) {
+							
+						}
+					});
+			}
+			
+//				async.eachSeries(list, function(room, callback2) {
+////					console.log('正在写入' + room.roomName + '的数据...');
+//					roomModle.insertRoom(room, function(result) {
+//						if (result.affectedRows == 1) {
+//							callback2();
+//						}
+//					});
+//				}, function(err) {
+//					if (err) return console.error(err.stack);
+//					console.log('处理成功！');
+//				});
+		}
+		
+		
+	});
+
+//	async.eachSeries([url], function(arr_url, callback) {
+//		request({
+//			url: arr_url,
+//			data: {
+//				pageno: 1
+//			}
+//		}, function(err, data, body) {
+//			if (err) return console.error(err);
+//			var total = JSON.parse(body).data.total;
 //
-//      request(options, function(err, data) {
-//          if (err) return console.error(err);
-//          
-//          console.log(data)
-//          var $ = cheerio.load(data.body.toString());
-//          $('#later-play-list .video-list-item').each(function() {
-//              var $me = $(this);
-//              var item = {
-//                  roomName : $me.find('.video-title').text(),
-//                  id : $me.attr('data-id'),
-//                  buzz :$me.find('.video-number').text(),
-//                  cate :$me.find('.video-cate').text(),
-//                  image :$me.find('img').attr('data-original')
-//              }
-//              list.push(item);
-//          });
-//          callback(err, list);
-//      });
-    }, function(err) {
-        if (err) return console.error(err.stack);
-        /*写入数据库*/
-        async.eachSeries(list, function(room, callback) {
-            console.log('正在写入' + room.roomName + '的数据...');
-            var sql = 'insert into ...';
-           roomModle.insertRoom(room,function(result){
-                if (result.affectedRows == 1) {
-                    callback(err);
-                }
-
-           });
-        }, function(err) {
-            if (err) return console.error(err.stack);
-            console.log('处理成功！');
-        });
-    });
+//			for (var a = 1; a <= total / 120; a++) {
+//				var uu = 'http://www.panda.tv/live_lists?status=2&order=person_num&pagenum=120&token=b902c644602f7b1ca7c6b2e0732f0d49&classification=&pageno=' + a;
+//				url_list.push(uu);
+//			}
+//
+//			callback(err, url_list);
+//		});
+//	}, function(err) {
+//		if (err) return console.error(err.stack);
+//		async.eachSeries(url_list, function(arr_url, callback) {
+//			console.log('正在抓取' + arr_url + '的数据...');
+//			request({
+//				url: arr_url
+//			}, function(err, data, body) {
+//				if (err) return console.error(err);
+//				var roomList = JSON.parse(body).data.items
+//				for (var i = 0; i < roomList.length; i++) {
+//					var _this = roomList[i];
+//					var item = {
+//						roomName: _this.userinfo.nickName,
+//						id: _this.id,
+//						buzz: _this.person_num,
+//						cate: _this.name,
+//						image: _this.pictures.img
+//					}
+//
+//					list.push(item);
+//				}
+//				console.log(list)
+//				callback(err, list);
+//			});
+//
+//		}, function(err) {
+//			async.eachSeries(list, function(room, callback) {
+//				console.log('正在写入' + room.roomName + '的数据...');
+//				roomModle.insertRoom(room, function(result) {
+//					if (result.affectedRows == 1) {
+//						callback(err);
+//					}
+//				});
+//			}, function(err) {
+//				if (err) return console.error(err.stack);
+//				console.log('处理成功！');
+//			});
+//		});
+//	});
 }
